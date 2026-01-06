0713cd62fa472cfd89b1c59e7883fbd9a89bfbcc
rn-santos227 <rnsantos227@gmail.com>
2026-01-07 01:09:37 +0800

    updated packages in frontend

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,20 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/pages/HomeView.vue'
-import NotFoundView from '@/pages/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      name: 'not-found',
-      component: NotFoundView,
-    },
+    //
   ],
 })
 
