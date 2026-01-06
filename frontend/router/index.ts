60a92c4eeb306f16acbed1ceda9e59961ad0ad02
rn-santos227 <rnsantos227@gmail.com>
2026-01-06 23:05:07 +0800

    updated vite config file

---
+++ frontend/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/pages/HomeView.vue'
+import NotFoundView from '@/pages/NotFoundView.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: NotFoundView,
+    },
+  ],
+})
+
+export default router
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'node:url'
+import { dirname, resolve } from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(dirname(fileURLToPath(import.meta.url)), 'src'),
+      '@type': resolve(dirname(fileURLToPath(import.meta.url)), 'src/@type'),
+    },
+  },
   plugins: [vue()],
 })
