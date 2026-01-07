6a791f7d95afa4057fe819c86a0acd1c8b2b6f9b
rn-santos227 <rnsantos227@gmail.com>
2026-01-07 21:16:06 +0800

    updated router index file

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,9 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/home/HomeView.vue'
+import PostDetailView from '@/views/post/PostDetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    //
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/posts/:id',
+      name: 'post-detail',
+      component: PostDetailView,
+    },
   ],
 })
 
